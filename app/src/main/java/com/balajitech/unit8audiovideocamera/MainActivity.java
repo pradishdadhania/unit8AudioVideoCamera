f95e1c422880888f8df35a17d19eba525e574f38
pradishdadhania <pradish.dadhania@gmail.com>
2022-04-05 11:32:21 +0530

    Initial commit

---
+++ app/src/main/java/com/balajitech/unit8audiovideocamera/MainActivity.java
@@ -0,0 +1,14 @@
+package com.balajitech.unit8audiovideocamera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
